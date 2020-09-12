1c6968531c0863f03fcc0c3e2c66eb7bb1580378
Florian Förster <foerster.florianjr@gmail.com>
2020-09-12 17:16:49 +0200

    feat: score system

--- main.py
+++ main.py
@@ -10,17 +10,20 @@ def run():
     command = input("Test (0)\nAppend (1)")
     if command == "0":
         rng = randint(0, len(voc) - 1)
-        used_voc = []
+        score = 0
+        current_attempts = 0
         while(True):
             random_voc = voc[rng]
-            print("Whats \"" + random_voc["plain"] + "\" in " + data["meta"]["translatedLanguage"])
+            print("Current Score: " + str(score))
+            print("Whats \"" + random_voc["plain"] + "\" in " + data["meta"]["translatedLanguage"] + "?")
             random_voc_answer = input("")
             if random_voc_answer == random_voc["translated"]:
                 print("You're right.")
+                score += round((10 /  (current_attempts + 1)))
                 rng = randint(0, len(voc) - 1)
             else:
                 print("Try again.")
-
+                current_attempts += 1
     else:
         to_append_plain = input("Plain:")
         to_append_trans = input("Translated:")
